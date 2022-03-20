d4320810afc500bacbde04b7a149b57d0ce8975f
Grzegorz Galios <grzegorz.galios@wp.pl>
2022-03-20 09:26:36 +0100

    Poprawa systemu kar i nagród. Uproszczenie pliku konfiguracji treningu - zostawienie konfiguracji minimalnej, reszta parametrów jest domyślna.

--- application/unity_mla_environment/MasterRacingEnvs/Assets/MyScripts/CarAgent.cs
+++ application/unity_mla_environment/MasterRacingEnvs/Assets/MyScripts/CarAgent.cs
@@ -6,6 +6,9 @@ using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using VehiclePhysics;
 
+/*
+    TODO - dobrać właściwe wartości dla stałych wykorzystywanych przy wyliczaniu nagród i kar!
+*/
 public class CarAgent : Agent
 {
 // ------------------------- Public data members. --------------------------- //
@@ -20,6 +23,15 @@ public class CarAgent : Agent
     public Transform Checkpoints;
 
 // ------------------------------ Methods. ---------------------------------- //
+    // Constructor
+    CarAgent() {
+        RAYS_ANGLES = new float[NUM_OF_RAYS_ANGLES];
+        float angleInterval = (MAX_RAY_ANGLE - MIN_RAY_ANGLE) / (NUM_OF_RAYS_ANGLES - 1);
+        for (int i = 0; i < NUM_OF_RAYS_ANGLES; ++i) {
+            RAYS_ANGLES[i] = MIN_RAY_ANGLE + i * angleInterval;
+        }
+    }
+
     // Start is called before the first frame update.
     void Start() {
         // Retrieve transform from CarAgentObject.
@@ -109,39 +121,59 @@ public class CarAgent : Agent
     }
 
     private void _computeStepRewards(float throttle, float steeringAngle) {
-        // // Add throttle reward.
-        // AddReward(THROTTLE_REWARD * throttle);
-        // // Add steering angle penalty.
-        // AddReward(STEERING_ANGLE_PENALTY * System.Math.Abs(steeringAngle));
+        // Add throttle reward.
+        AddReward(THROTTLE_REWARD * throttle);
+        // Add steering angle penalty.
+        AddReward(STEERING_ANGLE_PENALTY * System.Math.Abs(steeringAngle));
         
-        // // Get number of side rays intersecting with the road.
-        // int roadIntersectRaysCount = _getRoadIntersectRaysCount();
+        // Get number of side rays intersecting with the road.
+        int roadIntersectRaysCount = _getRoadIntersectRaysCount();
         
-        // // Add out-of-road penalty or road-center-distance reward.
-        // if (_isOutOfRoad()) {
-        //     // Add penalty based on how far out of road the car is.
-        //     AddReward(OUT_OF_ROAD_PENALTY * 0.2f * (5 - roadIntersectRaysCount));
-        // } else {
-        //     // Add reward based on how close to the center of road the car is.
-        //     AddReward(ROAD_CENTER_DISTANCE_REWARD * 0.1f * roadIntersectRaysCount);
-        // }
+        // Add out-of-road penalty or road-center-distance reward.
+        if (_isOutOfRoad()) {
+            // Add penalty based on how far out of road the car is.
+            int numOfSideRays = RAYS_ANGLES.Length / 2;
+            AddReward(OUT_OF_ROAD_PENALTY * (1.0f / numOfSideRays) * (numOfSideRays - roadIntersectRaysCount));
+        } else {
+            // Add reward based on how close to the center of road the car is.
+            AddReward(ROAD_CENTER_DISTANCE_REWARD * (1.0f / RAYS_ANGLES.Length) * roadIntersectRaysCount);
+        }
 
-        // // Add driving direction reward (or penalty)
-        // AddReward(CAR_DIRECTION_REWARD * _getDirectionScalar());
+        // Add driving direction reward (or penalty).
+        AddReward(CAR_DIRECTION_REWARD * _getDirectionScalar());
 
-        // // Add step penalty.
-        // AddReward(STEP_PENALTY);
+        // Add step penalty.
+        AddReward(STEP_PENALTY);
     }
 
     /*
         Get number of side car rays, which intersect with the road.
     */
     private int _getRoadIntersectRaysCount() {
-        // TODO - implementacja.
-        //##############
-        // throw new System.NotImplementedException();
-        return 5;
-        //##############
+        int intersectRaysCount = 0;
+        for (int i = 0; i < RAYS_ANGLES.Length; ++i) {
+            float angle = RAYS_ANGLES[i];
+            Vector3 leftRayDir =
+                    Quaternion.AngleAxis(angle, m_comTransform.forward) * -(m_comTransform.up);
+            Vector3 rightRayDir =
+                    Quaternion.AngleAxis(-angle, m_comTransform.forward) * -(m_comTransform.up);
+            if (Physics.Raycast(
+                    m_comTransform.position,
+                    leftRayDir,
+                    5.0f,
+                    RACE_TRACK_LAYER_MASK)) {
+                intersectRaysCount += 1;
+            }
+            if (Physics.Raycast(
+                    m_comTransform.position,
+                    rightRayDir,
+                    5.0f,
+                    RACE_TRACK_LAYER_MASK)) {
+                intersectRaysCount += 1;
+            }
+        }
+
+        return intersectRaysCount;
     }
     /*
         Check if car is outside of road - it's achieved by checking, if center
@@ -217,18 +249,23 @@ public class CarAgent : Agent
     private int m_nextCheckpointIdx = 0;
     // Race track layer mask constant.
     private const int RACE_TRACK_LAYER_MASK = 1 << 8;
+    // Array of angles between car rays and -(m_comTransform.up) direction vector.
+    private readonly float[] RAYS_ANGLES = { 72.5f, 73.5f, 74.5f, 75.5f, 76.5f, 77.5f, 78.5f, 79.5f, 80.5f, 81.5f, 82.5f, 83.5f, 84.5f, 85.5f };
+    private const float MIN_RAY_ANGLE = 72.5f;
+    private const float MAX_RAY_ANGLE = 85.5f;
+    private const int NUM_OF_RAYS_ANGLES = 14;
 
 // --------------------- Reward and penalty constants. ---------------------- //
     // Encourage to accelerate rather than brake.
-    private const float THROTTLE_REWARD = 0.5f;
+    private const float THROTTLE_REWARD = 0.25f;
     // Discourage to do redundant steering wheel movements.
-    private const float STEERING_ANGLE_PENALTY = -0.5f;
+    private const float STEERING_ANGLE_PENALTY = -0.25f;
     // Discourage to pull off the road.
-    private const float OUT_OF_ROAD_PENALTY = -1.0f;
+    private const float OUT_OF_ROAD_PENALTY = -0.25f;
     // Encourage to keep car on the center of the road.
-    private const float ROAD_CENTER_DISTANCE_REWARD = 0.5f;
+    private const float ROAD_CENTER_DISTANCE_REWARD = 0.25f;
     // Encourage to keep the correct driving direction.
-    private const float CAR_DIRECTION_REWARD = 0.5f;
+    private const float CAR_DIRECTION_REWARD = 0.25f;
     // Encourage to minimize steps needed to finish episode.
     private const float STEP_PENALTY = -0.05f;
 }
